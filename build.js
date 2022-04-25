da9b2126556b71fe2a0fa989cedeb61455aefb31
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2022-04-25 08:50:28 -0300

    atualização de dependencias

--- build.js
+++ build.js
@@ -1,41 +1,41 @@
 const StyleDictionary = require("style-dictionary").extend({
   source: ["src/**/*.json"],
   platforms: {
-      scss: {
-          transformGroup: "scss",
-          buildPath: "build/web/",
-          files: [
-              {
-                  destination: "_colors.scss",
-                  format: "scss/variables",
-                  filter: {
-                      type: "color"
-                  }
-              },
-              {
-                  destination: "_typography.scss",
-                  format: "scss/variables",
-                  filter: {
-                      type: "typography"
-                  }
-              },
-              {
-                  destination: "_grids.scss",
-                  format: "scss/variables",
-                  filter: {
-                      type: "grids"
-                  }
-              },
-              {
-                  destination: "_spacers.scss",
-                  format: "scss/variables",
-                  filter: {
-                      type: "spacers"
-                  }
-              }
-          ]
-      }
-  }
+    scss: {
+      transformGroup: "scss",
+      buildPath: "build/web/",
+      files: [
+        {
+          destination: "_colors.scss",
+          format: "scss/variables",
+          filter: {
+            type: "color",
+          },
+        },
+        {
+          destination: "_typography.scss",
+          format: "scss/variables",
+          filter: {
+            type: "typography",
+          },
+        },
+        {
+          destination: "_grids.scss",
+          format: "scss/variables",
+          filter: {
+            type: "grids",
+          },
+        },
+        {
+          destination: "_spacers.scss",
+          format: "scss/variables",
+          filter: {
+            type: "spacers",
+          },
+        },
+      ],
+    },
+  },
 });
 
 StyleDictionary.buildAllPlatforms();
